1015deed44ec447eddcf16e7f8c00d8feb71797e
Brandon Green <brandon@brandonleeproductions.com>
2020-01-25 14:38:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,11 +2,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  directors_index = 0 
+  total
   result = {
   }
   #
